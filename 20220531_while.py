ed30af71196317990be45e15772556bc87d100ea
yscho010612@gmail.com <yscho010612@gmail.com>
2022-05-31 09:18:37 +0900

    반복문 연습 while True

---
+++ 20220531_while.py
@@ -0,0 +1,31 @@
+'''
+JAVA
+while(조건식){
+    실행문;
+    증감식;
+}
+
+python
+while 조건식:
+    실행문
+    증감식
+'''
+
+x = 3
+while x < 5:
+    print(x)
+    x+=1
+    #무한 루프 멈추는 방법 ctrl+c    KeyboardInterrupt
+print('-'*20)
+# echo = input()
+# while echo != 'exit':
+#     print(echo)
+#     echo = input()
+print('-'*20)
+echo = input()
+while True:
+    if echo == 'exit':
+        break
+    print(echo)
+    echo = input()
+
