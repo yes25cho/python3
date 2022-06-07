35cacd1e6b7a83dde0cbc920c782350087ae83b8
yscho010612@gmail.com <yscho010612@gmail.com>
2022-06-07 10:31:05 +0900

    Quiz1.N-sum 문자열로 바꿔서 합계 계산

--- 20220607_Quiz.py
+++ 20220607_Quiz.py
@@ -1,11 +1,11 @@
 # 1. 휴대전화번호 입력하면 -, /, 띄어쓰기 없애고 숫자만 출력 하자
 phone_number = '010-7240-4658'
 #>> 01072404658
-for str in phone_number:
+for str1 in phone_number:
     if str == '-'or str == '/' or str == ' ':
         continue
     else:
-        print(str, end='')
+        print(str1, end='')
 print()
 # 2. 학번 -> 학년, 반, 학과, 번호 출력하기
 student_number = '2204'
@@ -30,8 +30,13 @@ while number!=0:
     number = number //10
 print(나머지)
 
-
-
+#문자 한자리씩 빼서 계산 하자
+number = 52323
+number_s = str(number)
+sum_num =0
+for n in number_s:
+    sum_num += int(n)
+print(sum_num)
 #4. 1~100 396게임을 출력
 # i=1
 # for i in range(1, 101):
