2182adb6f66058a3887e187fafee5d349b2a2737
yscho010612@gmail.com <yscho010612@gmail.com>
2022-10-25 09:43:43 +0900

    Drink __init__(), __str__(), 기본 메서드 저장

--- amasvin/drink.py
+++ amasvin/drink.py
@@ -12,3 +12,20 @@
 # 펄 : 타피오카, 알로에, 곤약, 코코넛
 # 당도 : 30%, 50%, 70%, 100%
 # 얼음량 : 없음, 적게, 기본, 많게
+
+class Drink:
+    #클래스 변수
+    _CUP_SIZES = ('레귤러', '점보')
+    _SUGARS = ('30%', '50%', '70%', '100%')
+    _ICES = ('없음', '적게', '기본', '많게')
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        self.cup_size = '레귤러'
+        self.suger = '50%'
+        self.ice = '기본'
+    def __str__(self):
+        return f'이름 : {self.name}\t가격 : {self.price}원\t컵사이즈 : {self.cup_size}\t당도 : {self.suger}\t얼음양 : {self.ice}'
+
+음료1 = Drink('아메리카노', 1800)
+print(음료1)
