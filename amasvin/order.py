66d71af45de0bf8f440da37d96581b1de1256c8e
yscho010612@gmail.com <yscho010612@gmail.com>
2022-11-08 10:21:05 +0900

    start order

---
+++ amasvin/order.py
@@ -0,0 +1,17 @@
+class Order:
+    def __init__(self):
+        #메뉴판
+        self.meun = []
+        self.init_menu()
+        #주문한 음료수 리스트
+        self.order_menu = []
+    def __str__(self):
+        pass
+    def init_meun(self):
+        pass
+    def order(self):
+        while():
+            self.show_maenu()
+
+    def show_menu(self):
+        pass
