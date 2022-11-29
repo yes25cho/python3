bf106c09f85bb9ca98a8bb8d9b59c7ed6be6c970
yscho010612@gmail.com <yscho010612@gmail.com>
2022-11-29 09:35:28 +0900

    import모듈; 모듈 함수() or 모듈 변수

---
+++ module, package/gugudan.py
@@ -0,0 +1,5 @@
+def gugudan(dan):
+    for i in range(1, 10):
+        print(f'{dan} x {i} = {dan * i}')
+if __name__ == '__main__':
+    gugudan(3)
--- module, package/insa2.py
+++ module, package/insa2.py
@@ -1,4 +1,6 @@
 import hello2
 import hello_func as h
+import gugudan as g#모듈의 이름
 
-h.hello()
+h.hello()[]
+g.gugudan(5)
